4a701da2aa10532654e3840de51993de9867deba
AlexisPescoran <alexisfer.18@gmail.com>
2019-04-07 23:26:44 -0500

    creación de componentes Navbar y Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/Header.css';
+
+export default function Header () {
+    return(
+        <header class="page-header">
+            <h1>BurgerQueen <small>te atendemos al instante</small></h1>
+        </header>
+    );
+}
